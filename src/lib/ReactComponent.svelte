c4dab32125e2495a4ca3fa3bbad6810cdf192ede
Fox <fox@foxdev.io>
2025-04-24 15:32:04 +0200

    ♻️ refactor(package): move version bump step before npm publish to not bump if build fails
    ♻️ refactor(ReactComponent): rename imported createRoot function to createReactRoot to not colision with svelte one

--- src/lib/ReactComponent.svelte
+++ src/lib/ReactComponent.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   // Source: https://gist.github.com/mquandalle/709e2ba691aa6dc6086a40df88bd4f1b
   import { createElement } from "react";
-  import { createRoot } from "react-dom/client";
+  import { createRoot as createReactRoot } from "react-dom/client";
   import { onMount, onDestroy } from "svelte";
 
   let container = $state(undefined as any as HTMLDivElement);
-  let reactRoot = $state(undefined as any as ReturnType<typeof createRoot>);
+  let reactRoot = $state(undefined as any as ReturnType<typeof createReactRoot>);
 
   const { this: el, children, class: classNames, ...props } = $props();
 
   onMount(() => {
-    reactRoot = createRoot(container);
+    reactRoot = createReactRoot(container);
   });
 
   $effect(() => {
